78f4c7352457fab9d47ac22892b545669ba816ab
carlos <cadriano.miranda@gmail.com>
2021-11-18 17:40:45 -0300

    data fetch to street

--- components/CrimeByStreet.tsx
+++ components/CrimeByStreet.tsx
@@ -1,55 +1,29 @@
 import React from "react";
+import { useEffectRequester } from "../hooks/useEffectRequester";
 import { CrimeByData } from "./CrimeByData";
 
-const fakeData = [
-  {
-    name: "Rua Itália (Prudente)",
-    crimes: 1600,
-  },
-  {
-    name: "Rua Paulista (Dracena)",
-    crimes: 1500,
-  },
-  {
-    name: "Rua Maria (Prudente)",
-    crimes: 1400,
-  },
-  {
-    name: "Rua João (Dracena)",
-    crimes: 1300,
-  },
-  {
-    name: "Rua Luvia (Dracena)",
-    crimes: 1200,
-  },
-  {
-    name: "Rua Josias (Prudente)",
-    crimes: 1100,
-  },
-  {
-    name: "Rua Jeremias (Prudente)",
-    crimes: 1000,
-  },
-  {
-    name: "Rua Santo (Prudente)",
-    crimes: 900,
-  },
-  {
-    name: "Rua Jeremias (Dracena)",
-    crimes: 1000,
-  },
-  {
-    name: "Rua Santo (Prudente)",
-    crimes: 900,
-  },
-];
+type TResponse = {
+  LOGRADOURO: string;
+  AMOUNT: number;
+}[];
+
+type TData = {
+  Logradouro: string;
+  Quantidade: number;
+}[];
 
 const CrimeByStreet = () => {
+  const { mappedData } = useEffectRequester<TResponse, TData>(
+    "/get_street_crime",
+    (data) =>
+      data.map((x) => ({ Quantidade: x.AMOUNT, Logradouro: x.LOGRADOURO }))
+  );
+
   return (
     <CrimeByData
-      data={fakeData}
-      xField="name"
-      yField="crimes"
+      data={mappedData}
+      xField="Logradouro"
+      yField="Quantidade"
       title="Top 10 ruas com a maior quantidade de registros de roubos"
     />
   );
