3568f57cd57882ea795eb7dd58d29956c78b5e6b
carlos <cadriano.miranda@gmail.com>
2021-11-18 10:22:39 -0300

    change filter order and removed time

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,5 +1,6 @@
 import { DatePicker, Input, Select } from "antd";
 import Grid from "@mui/material/Grid";
+
 const { RangePicker } = DatePicker;
 const { Option } = Select;
 const { Search } = Input;
@@ -7,9 +8,6 @@ const { Search } = Input;
 const Filters = () => {
   return (
     <Grid className="crime-filter-container" container spacing={1}>
-      <Grid item xs={12} md={4}>
-        <Search placeholder="Endereço" allowClear />
-      </Grid>
       <Grid item xs={12} md={4}>
         <Select className="crime-is-fullwidth" placeholder="Cidade">
           <Option value="time">Time</Option>
@@ -20,16 +18,21 @@ const Filters = () => {
           <Option value="year">Year</Option>
         </Select>
       </Grid>
+      <Grid item xs={12} md={4}>
+        <Search placeholder="Endereço" allowClear />
+      </Grid>
       <Grid item xs={12} md={2}>
         <RangePicker format="D/MM/YYYY" className="crime-is-fullwidth" />
       </Grid>
-      <Grid item xs={12} md={2}>
-        <RangePicker
-          placeholder={["Hora inicial", "Hora final"]}
-          format="HH:mm"
-          picker="time"
-          className="crime-is-fullwidth"
-        />
+      <Grid item xs={12} md={4}>
+        <Select className="crime-is-fullwidth" placeholder="Tipo de crime">
+          <Option value="time">Time</Option>
+          <Option value="date">Date</Option>
+          <Option value="week">Week</Option>
+          <Option value="month">Month</Option>
+          <Option value="quarter">Quarter</Option>
+          <Option value="year">Year</Option>
+        </Select>
       </Grid>
     </Grid>
   );
