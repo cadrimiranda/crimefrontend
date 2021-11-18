1a4579e8692de5554667ea97786a998fa1cc71e7
carlos <cadriano.miranda@gmail.com>
2021-11-18 17:41:11 -0300

    removed fake data from city chart

--- components/CrimeByCity.tsx
+++ components/CrimeByCity.tsx
@@ -2,56 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useEffectRequester } from "../hooks/useEffectRequester";
 import { CrimeByData } from "./CrimeByData";
 
-const fakeData = [
-  {
-    name: "Pres. Prudente",
-    crimes: 1800,
-  },
-  {
-    name: "Dracena",
-    crimes: 11900,
-  },
-  {
-    name: "Pres. Prudente1",
-    crimes: 1800,
-  },
-  {
-    name: "Dracena1",
-    crimes: 11900,
-  },
-  {
-    name: "Pres. Prudente2",
-    crimes: 1800,
-  },
-  {
-    name: "Dracena2",
-    crimes: 11900,
-  },
-  {
-    name: "Pres. Prudente11",
-    crimes: 1800,
-  },
-  {
-    name: "Dracena11",
-    crimes: 11900,
-  },
-  {
-    name: "Pres. Prudente12",
-    crimes: 1800,
-  },
-  {
-    name: "Dracena12",
-    crimes: 11900,
-  },
-  {
-    name: "Pres. Prudente21",
-    crimes: 1800,
-  },
-  {
-    name: "Dracena21",
-    crimes: 11900,
-  },
-];
 type TState = { Cidade: string; Quantidade: number };
 
 const CrimeByCity = () => {
