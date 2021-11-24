b853e55d1f587e4dd9de6a48db6f34b3cc288355
carlos <cadriano.miranda@gmail.com>
2021-11-23 22:01:19 -0300

    kpis

--- components/CrimeKpis.tsx
+++ components/CrimeKpis.tsx
@@ -1,18 +1,54 @@
+import Skeleton from "antd/lib/skeleton";
+import { useEffectRequester } from "../hooks/useEffectRequester";
 import { DataCircle } from "./DataCircle";
+import { useFilterContext } from "./FilterProvider";
+import { CrimeEnum } from "./Filters";
+
+type TResponse = {
+  TYPE: CrimeEnum;
+  AMOUNT: number;
+};
 
 const CrimeKpis = () => {
+  const { city } = useFilterContext();
+  const { data, loading } =
+    useEffectRequester<TResponse[]>("/get_amount_crime");
+
+  function getCircle(x: TResponse) {
+    let name = "";
+    switch (x.TYPE) {
+      case CrimeEnum.btnFurtoCelular:
+        name = "Furtos de celulares";
+        break;
+      case CrimeEnum.btnFurtoVeiculo:
+        name = "Furtos de veiculos";
+        break;
+      case CrimeEnum.btnRouboCelular:
+        name = "Roubos de celulares";
+        break;
+      case CrimeEnum.btnRouboVeiculo:
+        name = "Roubos de veiculos";
+        break;
+    }
+
+    return <DataCircle key={name} value={x.AMOUNT} text={name} />;
+  }
+
   return (
     <div className="crime-kpi-container crime-card-only-desktop">
       <h1 className="crime-title-with-subtitle crime-only-desktop">
-        Aqui vai um titulo para os KPIs que ainda nao pensei
+        {`Acumulado de roubos e furtos${city ? ` em ${city}` : ""}`}
       </h1>
-      <h2 className="crime-subtitle crime-only-desktop">
-        Preciso ver como exibir, no mobile tá as bolinhas
-      </h2>
       <div className="crime-kpi-itens crime-pos-flex crime-pos-center-around">
-        <DataCircle text="1800 crime" />
-        <DataCircle text="1800 crime" />
-        <DataCircle text="1800 crime" />
+        {Array.isArray(data) && !loading && data.map(getCircle)}
+        {loading &&
+          [1, 2, 3, 4].map(() => (
+            <Skeleton.Button
+              style={{ height: "115px", width: "115px", margin: "6px 0" }}
+              active
+              shape="round"
+            />
+          ))}
       </div>
     </div>
   );
--- components/DataCircle.tsx
+++ components/DataCircle.tsx
@@ -1,6 +1,7 @@
-const DataCircle = ({ text }: { text: string }) => {
+const DataCircle = ({ value, text }: { value: number; text: string }) => {
   return (
-    <div className="crime-datacircle crime-pos-flex crime-pos-center-center">
+    <div className="crime-datacircle crime-pos-flex crime-pos-center-center crime-pos-column">
+      <span className="crime-datacircle-value">{value}</span>
       <span>{text}</span>
     </div>
   );
