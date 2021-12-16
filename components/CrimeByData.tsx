e8d20622b65d37cdb35ccd478ef011ed59085192
carlos <cadriano.miranda@gmail.com>
2021-12-16 20:16:58 -0300

    use classnames to combine with props

--- components/CrimeByData.tsx
+++ components/CrimeByData.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 import { ColumnConfig } from "@ant-design/charts";
 import Spin from "antd/lib/spin";
+import cx from "classnames";
 
 const Column = dynamic<ColumnConfig>(
   () => import("@ant-design/charts").then((mod) => mod.Column) as any,
@@ -14,6 +15,7 @@ interface ICrimeByData {
   yField: string;
   title: string;
   loading: boolean;
+  classes?: string;
 }
 
 export const LoadingGraphic = () => (
@@ -28,9 +30,11 @@ const CrimeByData = ({
   yField,
   title,
   loading,
+  classes,
 }: ICrimeByData) => {
+  const allClasses = cx("crime-card", classes);
   return (
-    <div className="crime-card">
+    <div className={allClasses}>
       <h1 className="crime-title">{title}</h1>
       <div style={{ position: "relative" }}>
         {loading && <LoadingGraphic />}
